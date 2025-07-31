d60b8d3f56f98038b21a6c30e3a91eb768f87897
Karolline Carvalho <103125420+KarollineCS@users.noreply.github.com>
2025-07-31 17:36:12 -0300

    Add files via upload

---
+++ problemas/lista_1/soma.cpp
@@ -0,0 +1,14 @@
+/*
+    Ex: Lista 1 (ECOI02.1)
+    Aluna: Karolline Carvalho Silva RA: 2020026008
+*/
+#include <iostream>
+using namespace std;
+
+main()
+{
+    float soma, v1 = 5.0000, v2 = 2.50000;
+    soma = v1 + v2;
+	cout << "A soma \nv1: " << v1 << "\nv2: " << v2 << "\neh igual a: " << soma;
+	return 0;
+}
---
+++ problemas/lista_1/soma2.cpp
@@ -0,0 +1,18 @@
+/*
+    Ex: Lista 1 (ECOI02.1)
+    Aluna: Karolline Carvalho Silva RA: 2020026008
+*/
+#include <iostream>
+using namespace std;
+
+main()
+{
+	float n1, n2, soma;
+	cout << "Entre com dois valores para serem somados! \nn1: ";
+	cin  >> n1;
+	cout << "n2: ";
+	cin  >> n2;
+	soma = n1 + n2;
+	cout << "A soma dos valores eh: " << soma << endl;
+	return 0;
+}
